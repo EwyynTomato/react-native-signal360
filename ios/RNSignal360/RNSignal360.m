e8d9969a2ff438f2aa230027ca8dc2bc89bf33ef
Erwin <elwyyntomato+tw@gmail.com>
2017-09-06 02:18:52 +0800

    fix: app crash upon Dev Menu > Reload

--- ios/RNSignal360/RNSignal360.m
+++ ios/RNSignal360/RNSignal360.m
@@ -1,6 +1,5 @@
-
 #import "RNSignal360.h"
-#import <Signal360/SignalCodeHeard.h>
+#import <Signal360/SignalBluetoothCodeHeard.h>
 
 @implementation RNSignal360
 
@@ -8,6 +7,7 @@
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(setGuid:(NSString *)guid) {
@@ -23,6 +23,7 @@ RCT_EXPORT_METHOD(start) {
     [[Signal sharedInstance] requestNotificationPermission];
 
     [[Signal sharedInstance] initializeWithApplicationGUID:self.deviceGuid andDelegate:self];
+    [[Signal sharedInstance] setDelegate:self]; //<- this is needed so that Dev Menu > Reload will replace the delegate with new instance, initializeWithApplicationGUID:andDelegate: doesn't seem to do such thing.
     [[Signal sharedInstance] start];
 }
 
@@ -31,9 +32,22 @@ RCT_EXPORT_METHOD(stop) {
 }
 
 - (BOOL)signal:(Signal *)signal didHearCode:(SignalCodeHeard *)code {
-    [self sendEventWithName:[self getEventName]
-                       body:@{@"signalType": @"AUDIO",
-                              @"beaconCode": [NSString stringWithFormat:@"%ld", code.beaconCode]}];
+    /**
+     We need to check if self.bridge still exists, for the following reasons Upon Dev Menu > Reload:
+     - _bridge will no longer exist, and self.bridge would be nil
+     - SignalDelegate would keep on calling this delegate method
+     - RCTEventEmitter's sendEventWithName need _bridge not to be nil to work/NOT CRASH
+     - this RCT Module won't be dealloc late after, so implementing [[Signal sharedInstance] stop]; in dealloc won't prevent the issue
+     
+     Oh, also: you can't move this delegate to AppDelegate, because you need to emit event using RCTEventEmitter, which is only available as instance method.
+     */
+    if (self.bridge) {
+        NSString *signalType = [code isKindOfClass:[SignalBluetoothCodeHeard class]] ? @"BLUETOOTH" : @"AUDIO";
+        [self sendEventWithName:[self getEventName]
+                           body:@{@"signalType": signalType,
+                                  @"beaconCode": [NSString stringWithFormat:@"%ld", code.beaconCode]}];
+    }
+    
     return YES;
 }
 
@@ -46,4 +60,4 @@ RCT_EXPORT_METHOD(stop) {
 }
 
 @end
-  
+
